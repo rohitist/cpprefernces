6b029e16f59e59f621427e4cbc9393045bbc3e8e
Rohit Kumar <Rohit.Kumar5@tatacommunications.com>
2019-12-03 03:08:09 -0500

    added files under oops

---
+++ oops/public_inheritence.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+class animal{
+public:
+	virtual void bark() { cout << "animal bark()\n"; }
+	virtual void eat() { cout << "animal eat()\n"; }
+};
+
+class dog : public animal{
+public:
+	void bark() { cout << "dog bark()\n"; }
+	void eat() { cout << "dog eat()\n"; }
+};
+
+int main(){
+	animal *a;
+	dog* d = new dog();
+
+	a = d;
+	a->bark();
+	a->eat();
+
+	return 0;
+}
