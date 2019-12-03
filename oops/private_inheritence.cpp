c0d6f0ca4566bfdd32c25d61886b80e50ec73aa3
Rohit Kumar <Rohit.Kumar5@tatacommunications.com>
2019-12-03 03:15:52 -0500

    added private_inheritence

---
+++ oops/private_inheritence.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+class animal{
+public:
+	virtual void bark() { cout << "animal bark()\n"; }
+	virtual void eat() { cout << "animal eat()\n"; }
+};
+
+class dog : private animal{ //private inheritence means all members of animal is private to dog
+public:
+	void bark() { cout << "dog bark()\n"; }
+	void eat() { cout << "dog eat()\n"; }
+};
+
+int main(){
+	animal *a;
+	dog* d = new dog();
+
+	a = d;
+	a->bark(); //trying to access private members of animal
+	a->eat(); //trying to access private members of animal
+
+	return 0;
+}
